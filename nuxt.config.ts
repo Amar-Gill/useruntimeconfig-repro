1f5e1b2cd2af3e8b6732dfdae3f9bddd75bfd53d
Amar Gill <amar.s.gill@outlook.com>
2024-11-16 12:42:29 -0500

    use process.env in nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   runtimeConfig: {
-    backendTest: "abc123",
-    public: { clientTest: "xyzabc" },
+    backendTest: process.env.NUXT_BACKEND_TEST,
+    public: { clientTest: process.env.NUXT_PUBLIC_CLIENT_TEST },
   },
 });
